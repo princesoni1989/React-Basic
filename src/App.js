f4e88836bd45da4aaffdc339e4a1ae692767d187
Prince Soni <prince.soni@tothenew.com>
2016-12-27 11:22:17 +0530

    Intial structure

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
+/**
+ * Created by ttnd on 27/12/16.
+ */
 import React from 'react'
-import {Form, FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap'
-import Courses from './component/courses'
-import {connect} from 'react-redux'
-require('./style.css')
 
-class App extends React.Component {
-  constructor(){
+export default class App extends React.Component {
+  constructor() {
     super()
-    this.incrementCount = this.incrementCount.bind(this)
-  }
-
-  incrementCount () {
-    this.setState({
-      count: this.state.count + 1
-    })
   }
 
   render() {
-    let {courses, surname, isFetching} = this.props
     return (
-      <div >
-        {isFetching ? <h1> Loading......</h1> : <Courses courses={courses}/>}
-
+      <div>
+        Working
       </div>
     )
   }
-}
 
-const mapStateToProps = (state, ownProps) => {
-  console.log("State>>>>>>>>>>", state)
- return {
-   courses: state.courses,
-   surname: ownProps.surname,
-   isFetching: state.ajax
- }
 }
-
-export default connect(mapStateToProps)(App)
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 /* eslint-disable no-console */
 import React from 'react'
-import {Provider} from 'react-redux'
 import {render} from 'react-dom';
 import App from './App'
-import store from './config'
-import {getCourses} from './actions'
-
-store.dispatch(getCourses())
 
 let app = document.getElementById('main')
-render(
-  <Provider store={store}>
-      <App surname="soni"/>
-  </Provider>, app)
+
+
+render(<App />, app)
